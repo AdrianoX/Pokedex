12645198a776b5e0d931981ef0f2c5cc39f384fe
Adrian Kryzinski <adriankryzinski@gmail.com>
2021-01-22 18:42:56 +0100

    Display issues fixed - adding & implementing makeStyles from  material-ui...

--- src/AllPokemons.js
+++ src/AllPokemons.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { AppBar, Toolbar, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+// import classes from "*.module.css";
+
+const columnStyles = makeStyles({
+  allPokemonsBox: {
+    paddingRight: "75px",
+    paddingLeft: "75px",
+    paddingTop: "35px",
+  },
+});
 
 const allPokemons = () => {
+  const classes = columnStyles();
   return (
     <>
       <AppBar position="static">
         <Toolbar />
       </AppBar>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className={classes.columnStyles}>
         <Grid item xs={4}>
           First Poke Column
         </Grid>
