166fa32958cb800ce8de881bfc142d83f99728ac
Adrian Kryzinski <adriankryzinski@gmail.com>
2021-01-22 18:39:16 +0100

    Creating basic grid structure for homePage (allPokemons) - little issues with display

--- src/AllPokemons.js
+++ src/AllPokemons.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { AppBar, Toolbar, Grid } from "@material-ui/core";
 
 const allPokemons = () => {
-  return <div> All pokemons page</div>;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar />
+      </AppBar>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          First Poke Column
+        </Grid>
+        <Grid item xs={4}>
+          Second Poke Column
+        </Grid>
+        <Grid item xs={4}>
+          Third Poke Column
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default allPokemons;
