d63ef6b327e5cdd503fd4603371da714561b7716
Adrian Kryzinski <adriankryzinski@gmail.com>
2021-01-25 18:38:30 +0100

    Start working on search bar

--- src/AllPokemons.js
+++ src/AllPokemons.js
@@ -14,6 +14,7 @@ import { makeStyles } from "@material-ui/core/styles";
 // import mainData from "./mainData";
 import { UpperCaseLetterName } from "./constVariables";
 import axios from "axios";
+import SearchIcon from "@material-ui/icons/Search";
 
 const columnStyles = makeStyles({
   allPokemonsBox: {
@@ -78,7 +79,11 @@ const AllPokemons = (props) => {
   return (
     <>
       <AppBar position="static">
-        <Toolbar />
+        <Toolbar>
+          <div>
+            <SearchIcon />
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.allPokemonsBox}>
